cdfdd9048cd9d19a41dca46717a82c7ce653f81b
Daravy DeRouen <daravyderouen@gmail.com>
2021-11-04 17:37:44 -0500

    Revised code for myCipher

--- part-1/myCipher.js
+++ part-1/myCipher.js
@@ -1,4 +1,43 @@
       //abcdefghijklmnopqrstuvwxyz
-let key=zyxwvutsrqponmlkjihgfedcba
+//let key=zyxwvutsrqponmlkjihgfedcba
 
-let firstPhrase ="I love cryptography";
+let phrase = "I LOVE CRYPTOGRAPHY"
+
+function rot13(str) {
+      const rot13 = {
+        'N': "A",
+        'O': 'B',
+        'P': 'C',
+        'Q': 'D',
+        'R': 'E',
+        'S': 'F',
+        'T': 'G',
+        'U': 'H',
+        'V': 'I',
+        'W': 'J',
+        'X': 'K',
+        'Y': 'L',
+        'Z': 'M',
+        'A': 'N',
+        'B': 'O',
+        'C': 'P',
+        'D': 'Q',
+        'E': 'R',
+        'F': 'S',
+        'G': 'T',
+        'H': 'U',
+        'I': 'V',
+        'J': 'W',
+        'K': 'X',
+        'L': 'Y',
+        'M': 'Z'
+      }
+      const splitStr = str.split(' ').map(string => {
+        return string.split('').map(string => rot13[string] === undefined ? string : rot13[string]).join('');
+      }).join(' ');
+        return splitStr;
+      }
+
+      console.log(rot13(phrase));
+
+      //I picked this one because it was easy to cipher. I would not actually use this one for my own because it's too easy to break. 
---
+++ part-1/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+let firstPhrase ="I love cryptography"
